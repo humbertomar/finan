0310a0c1878801d929d66f4a730d3286f26bee6e
Betim <betimmariano@hotmail.com>
2025-12-10 15:34:27 -0300

    new docker

--- expense-backend/src/prisma/prisma.service.ts
+++ expense-backend/src/prisma/prisma.service.ts
@@ -1,21 +1,19 @@
+// src/prisma/prisma.service.ts
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
+import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy {
-  constructor(configService: ConfigService) {
-    const connectionString = configService.get<string>('DATABASE_URL');
-    const pool = new Pool({ connectionString });
-    const adapter = new PrismaPg(pool);
-
-    super({
-      adapter,
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
     });
+
+    // Prisma 7 exige adapter ou accelerateUrl
+    super({ adapter });
   }
 
   async onModuleInit() {
