d749a1b00b6d274fc32df2fb05cf6cb83b2a2a01
Betim <betimmariano@hotmail.com>
2025-12-11 16:23:03 -0300

    ajuste frontback

--- expense-backend/src/main.ts
+++ expense-backend/src/main.ts
@@ -4,8 +4,17 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors();
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',      // dev front
+      'https://finan.humberto.dev.br', // front em produção
+    ],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT || 3050);
 }
 bootstrap();
